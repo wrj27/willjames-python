94a06044565fbf059b25908a56544775178ba506
YimoShen.java <19shenyimo@bprep.org>
2018-09-11 09:17:46 -0700

    Unit2blab

---
+++ Unit2BLab/Unit2BLab.py
@@ -0,0 +1,19 @@
+numGrade = input ("numerical grade")
+if (int (numGrade)) >= 90:
+    print ("Yahoo you got an A!")
+elif (int (numGrade)) >= 80:
+    print ("Good you got a B.")
+elif (int (numGrade)) >= 70:
+    print ("Okay you got a C.")
+else: print ("You better study harder.")
+
+userinput = input("what is your favorite color")
+if userinput == "blue":
+    print ("The sky is blue.")
+elif userinput == "green":
+    print ("The forest is green.")
+elif userinput == "red":
+    print ("I love red.")
+elif userinput == "yellow":
+    print ("Yuck Yellow.")
+else: print ("I am a dumb computer-I only know four colors.")
