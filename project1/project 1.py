1f46e0298c4d7e6afcfb106d536a6a07fdef1330
YimoShen.java <19shenyimo@bprep.org>
2018-09-24 09:19:11 -0700

    Project 1

---
+++ project1/project 1.py
@@ -0,0 +1,28 @@
+#def top(myGrade, letterGrades):
+    #print ("I am a " + str(longGrade))
+
+def longNameGrade():
+    input (int("What grade are you in?"))
+    if 9:
+        print ("I am a Freshman")
+    elif 10:
+        print ("I am a Sophomore")
+    elif 11:
+        print ("I am a Junior")
+    elif 12:
+        print ("I am a Senior")
+    else:
+        print ("What are you doing here?")
+
+def myAverageGrades():
+    gradeList = [89.2, 95.7, 92.4, 86.1]
+    print (len (gradeList))
+    print (float(89.2 + 95.7 + 92.4 + 86.1))
+
+
+def successRate():
+    get average
+    if average >= 70:
+        print ("Yay! You're passing")
+    else average < 70:
+        print ("You are a failure")
