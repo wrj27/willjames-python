f7caebd7281cfa1b64bf12555d64190c56a3f8fa
YimoShen.java <19shenyimo@bprep.org>
2018-09-28 08:50:11 -0700

    project 1.1

--- project1.1/project1.1.py
+++ project1.1/project1.1.py
@@ -1,41 +1,43 @@
 def main():
-    print ("start here")
-
-    input ("") #add a title for this input
-
-    getYearInSchool (z) #calling back the get yearInSchool Function with input as argument
-    #argument gets passed to get yearInSchool
-    getYearInSchool ()
-
-    print ("I am a " + str(longYearInSchool))
+    grade = int(input ("What grade are you in? - "))
+    print (getYearInSchool(grade))
 
     myGradesList = [92.4, 97.8, 85.6, 87.1]
-    averageGrade #calling the average grade function with average grade as argument
-    #pass the aarguments to get the average grade
-    calcAverageGrade ()
+    averages = calcAverageGrade(myGradesList)
+    print ("Your average grade is", averages)
 
-    print (averageGrade)
+    print ("Your letter grade is", getLetterGrade(averages))
 
-    #call the letter grade function with average grade as the argument
-    #1 argument gets passed to getLetterGrade
-    getLetterGrade()
 
-    print (letterGrade)
+def getYearInSchool(grade):
+    if grade == 9:
+        return ("Freshman")
+    elif grade == 10:
+        return ("Sophomore")
+    elif grade == 11:
+        return ("Junior")
+    elif grade == 12:
+        return ("Senior")
+    else:
+        return ('Not in highschool.')
 
-    #determine if letter grade is passing or failing
+def calcAverageGrade(myGradesList):
+    average = float (((myGradesList[0] + myGradesList[1] + myGradesList[2] + myGradesList[3]) / len(myGradesList)))
+    return average
 
-def getYearInSchool (z):
-    print ("getYearInSchool")
-    myLongGrade = input ("What is your year in school?")
-    return longGrade
-
-def calcAverageGrade(x,y):
-    print ("calcAverageGrade")
-    return calcAverageGrade
-
-def getLetterGrade(w):
-    print ("getLetterGrade")
-    return getLetterGrade
+def getLetterGrade(averages):
+    if averages >= 90:
+        return "A"
+    elif averages >= 80:
+        return "B"
+    elif averages >= 70:
+        return "C"
+    elif averages >= 60:
+        return "D"
+    elif 0<= averages < 60:
+        return "F"
+    else:
+        return "get smart"
 
 
 main()
