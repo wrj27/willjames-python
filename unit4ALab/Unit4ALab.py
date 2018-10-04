c63b92e1997bd2d204b58e1552bde860a0cb4eb3
YimoShen.java <19shenyimo@bprep.org>
2018-10-04 09:01:37 -0700

    Unit 4A

--- unit4ALab/Unit4ALab.py
+++ unit4ALab/Unit4ALab.py
@@ -1,6 +1,19 @@
-def devowel(word):
-    someWord = input ("Pick a word, any word.")
+def main():
+    someWord = str (input ("Pick a word, any word."))
     print (someWord)
 
+    print(devowel(someWord))
 
-devowel()
+
+
+
+def devowel(someWord):
+    someWord2 = someWord
+    noVowel = ""
+    for w in someWord2:
+        if (w != "a" and w != "e" and w != "i" and w != "o" and w != "u"):
+            noVowel = noVowel + w
+    return (noVowel)
+
+
+main()
