d3c8906574091e86894e07a0acd587e9219df8d4
YimoShen.java <19shenyimo@bprep.org>
2018-10-23 09:02:17 -0700

    Unit4D

--- Unit4DLab/Unit4DLab.py
+++ Unit4DLab/Unit4DLab.py
@@ -1,15 +1,29 @@
 def main():
     shoppingCart = [["toothpaste", "qtips", "milk"], ["milk", "candy", "apples"], ["paper", "pencils", "qtips"]]
-    print (allInOne(myShoppingCart))
+    print (allInOne(shoppingCart))
+    print (countQtips(shoppingCart))
 
 
-def allInOne(myShoppingCart):
+def allInOne(shoppingCart):
     newList = []
     for list in shoppingCart:
         for item in list:
             if item not in newList:
                 newList.append (item)
     return (newList)
+
+def countQtips(shoppingCart):
+    list2 = []
+    total = 0
+    for list in shoppingCart:
+        for item in list:
+            if item not in shoppingCart:
+                list2.append(item)
+    for w in range(len(list2)):
+        if list2[w] == "qtips":
+            total += 1
+    return (total)
+
 
 
 
