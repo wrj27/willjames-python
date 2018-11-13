3aee3288e3faea079fc23eb393593ba2df4f02bd
YimoShen.java <19shenyimo@bprep.org>
2018-11-13 08:11:06 -0800

    Turtle

--- turtleINTRO/Turtle intro.py
+++ turtleINTRO/Turtle intro.py
@@ -1,10 +1,69 @@
 import turtle
 
 pen = turtle.Pen()
+turtle.bgcolor("black")
+pen.speed(1)
 
-def firstDrawing():
-    for x in range (55):
-        pen.forward(x)
-        pen.left(angle)
+def main():
+    firstSquare()
+    secondSquare()
+    rectangle1()
+    rectangle2()
+    turtle.exitonclick()
 
-firstDrawing(25)
+
+
+def firstSquare():
+    pen.color(1.0, 0.0, 0.0)
+    pen.pendown()
+    pen.forward(20)
+    pen.left(angle = 90)
+    pen.forward(20)
+    pen.left(angle = 90)
+    pen.forward(20)
+    pen.left(angle = 90)
+    pen.forward(20)
+    pen.left(angle = 90)
+    pen.penup()
+
+def secondSquare():
+    pen.color(.16, .45, .1)
+    pen.pendown()
+    pen.forward(100)
+    pen.right(angle = 90)
+    pen.forward(100)
+    pen.right(angle = 90)
+    pen.forward(100)
+    pen.right(angle = 90)
+    pen.forward(100)
+    pen.right(angle = 90)
+    pen.penup()
+
+def rectangle1():
+    pen.color(.0, 1.0, .0)
+    pen.pendown()
+    pen.left(angle = 180)
+    pen.forward(35)
+    pen.left(angle = 90)
+    pen.forward(55)
+    pen.left(angle = 90)
+    pen.forward(35)
+    pen.left(angle = 90)
+    pen.forward(55)
+    pen.penup()
+
+def rectangle2():
+    pen.color(.34, .87, 1.0)
+    pen.pendown()
+    pen.forward(250)
+    pen.left(angle = 90)
+    pen.forward(100)
+    pen.left(angle = 90)
+    pen.forward(250)
+    pen.left(angle = 90)
+    pen.forward(100)
+
+
+
+
+main()
