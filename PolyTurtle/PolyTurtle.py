0892f9a14174b6be3ab6402e9631e455693f110d
YimoShen.java <19shenyimo@bprep.org>
2018-11-15 09:05:02 -0800

    Poly Turtle

---
+++ PolyTurtle/PolyTurtle.py
@@ -0,0 +1,28 @@
+import turtle
+
+pen = turtle.Pen()
+turtle.setup(width = 1000, height = 750)
+turtle.title("Poly Turtle")
+pen.speed(1)
+turtle.bgcolor(input("Your background color is -"))
+
+def mainDraw():
+    side = int(input("What is the length of your sides? - "))
+    num = int(input("How many sides does your shape have? - "))
+    color = input("What color is your pen? - ")
+    pen.pencolor(str(color))
+    x = 0
+    z = int(360 / num)
+    #y = int(((num-2) * 360) / (num))
+    while x < num:
+        pen.pendown()
+        pen.forward(side)
+        pen.left(angle = y)
+        pen.penup()
+        x += 1
+
+
+
+
+mainDraw()
+turtle.exitonclick()
