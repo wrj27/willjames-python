e3940b4ace0799b898923493867f31d1096df556
YimoShen.java <19shenyimo@bprep.org>
2018-12-14 08:41:58 -0800

    Unit 7A Lab

--- 7ALab/Unit 7A Lab.py
+++ 7ALab/Unit 7A Lab.py
@@ -46,6 +46,88 @@ else:
 print(myCage.getcName())
 
 
+class myPet1:
+    def __init__(self, dType, dName, dBreed):
+        self.type = dType
+        self.name = dName
+        self.breed = dBreed
+    def getDType(self):
+        return (self.type)
+    def getDName(self):
+        return (self.name)
+    def getDBreed(self):
+        return (self.breed)
 
+myDog = myPet1("Dog", input("Give name. - "), input("What breed?"))
+print(myDog.getDType())
+print(myDog.getDName())
+print(myDog.getDBreed())
+
+
+class myPet2:
+    def __init__(self, zType, zName, zBreed):
+        self.type = zType
+        self.name = zName
+        self.breed = zBreed
+    def getZType(self):
+        return (self.type)
+    def getZName(self):
+        return (self.name)
+    def getZBreed(self):
+        return (self.breed)
+
+myCat = myPet2("Cat", input("Give name. - "), input("What breed?"))
+print(myCat.getZType())
+print(myCat.getZName())
+print(myCat.getZBreed())
+
+
+
+class cagePet1():
+    def __init__(self, cType, cDanger, cName):
+        self.type2 = cType
+        self.danger = cDanger
+        self.name2 = cName
+    def getcType(self):
+        return (self.type2)
+    def getDanger(self):
+        return (self.danger)
+    def getcName(self):
+        return (self.name2)
+b = "t"
+
+myCage1 = cagePet1("Snake", b, input("Name it. - "))
+print(myCage1.getcType())
+if b is "t":
+    print("This animal is dangerous.")
+elif b is "f":
+    print("This animal is not dangerous.")
+else:
+    "y"
+print(myCage1.getcName())
+
+
+class cagePet2():
+    def __init__(self, cType, cDanger, cName):
+        self.type2 = cType
+        self.danger = cDanger
+        self.name2 = cName
+    def getcType(self):
+        return (self.type2)
+    def getDanger(self):
+        return (self.danger)
+    def getcName(self):
+        return (self.name2)
+
+a = "f"
+myCage2 = cagePet2("Rat", a, input("Name it. - "))
+print(myCage2.getcType())
+if a is "t":
+    print("This animal is dangerous.")
+elif a is "f":
+    print("This animal is not dangerous.")
+else:
+    "y"
+print(myCage2.getcName())
 
 
